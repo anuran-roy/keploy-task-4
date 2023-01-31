4b5ea97a9f19e9318888e28620a4a7b8132da46f
Anuran Roy <anuranroy02@gmail.com>
2023-01-31 19:47:32 +0530

    First prototype

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const getJoke = require("./script");
+
+app = express();
+app.use(cors());
+
+app.get("/", async (api_req, api_res) => {
+    output_type = api_req.query.type;
+    console.log(output_type);
+    joke_res = await getJoke("Programming");
+    api_res.send({
+        "joke": joke_res,
+    })
+})
+
+app.listen(3000, () => console.log("Listening on port 3000"));
